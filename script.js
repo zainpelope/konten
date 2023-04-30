5d710a4c87daaab565cf9e6d302aa456849f6161
zainpelope <palengaancity12345@gmail.com>
2023-04-30 09:34:38 +0700

    okeyh

--- script.js
+++ script.js
@@ -1,57 +1,59 @@
 const kalimat = [
-	"Apa Kabar",
-	"Di sini awal kita bertemu",
-	"Seiring berjalannya waktu",
-	"Aku punya rasa terhadapmu",
-	"Aku mengungkapkan Dan kita jadian",
-	"Pada Akhirnya",
-	"Kamu memilih pergi",
-	"Belum selesai dengan masa lalu",
-	"Dan melanjutkan kisah",
-	"Dan disini Ku bisa bersabar Dan menunggu mu",
-	"Dalam kebingungan",
-	"Yang tetap mencintaimu Dan sayang kepadamu",
-	"Kamu tidak jujur tentang itu",
-	"Semoga bahagia",
-	"Dan seandainya air jatuh kembali",
-	"Aku tetap di sini Dan akan mengusap kembali air mata itu",
-	"Karena di sini aku tulus mencintaimu"
-];
-
-const gambar = [
-	"1.gif",
-	"2.gif",
-	"3.gif",
-	"4.gif",
-	"5.gif",
-	"6.gif",
-	"7.gif",
-	"8.gif",
-	"9.gif",
-	"10.gif",
-	"11.gif",
-	"12.gif",
-	"13.gif",
-	"14.gif",
-	"15.gif",
-	"16.gif",
-	"17.gif",
-	"18.gif"
-];
-
-const audio = document.getElementById("audio");
-const kalimatElem = document.getElementById("kalimat");
-const gambarElem = document.getElementById("gambar");
-
-let currentIndex = 0;
-
-kalimatElem.addEventListener("click", function() {
-	currentIndex++;
-	if (currentIndex === kalimat.length) {
-		currentIndex = 0;
-	}
-
-	kalimatElem.innerText = kalimat[currentIndex];
-	gambarElem.src = gambar[currentIndex];
-	audio.play();
-});
+    "&ldquo;Aduh Maaf&rdquo;",
+    "&ldquo;Apa Kabar&rdquo;",
+    "&ldquo;Di sini awal kita bertemu&rdquo;",
+    "&ldquo;Seiring berjalannya waktu&rdquo;",
+    "&ldquo;Aku punya rasa terhadapmu&rdquo;",
+    "&ldquo;Aku mengungkapkan Dan kita jadian&rdquo;",
+    "&ldquo;Pada Akhirnya&rdquo;",
+    "&ldquo;Kamu memilih pergi&rdquo;",
+    "&ldquo;Belum selesai dengan masa lalu&rdquo;",
+    "&ldquo;Dan melanjutkan kisah&rdquo;",
+    "&ldquo;Dan disini Ku bisa bersabar Dan menunggu mu&rdquo;",
+    "&ldquo;Dalam kebingungan&rdquo;",
+    "&ldquo;Yang tetap mencintaimu Dan sayang kepadamu&rdquo;",
+    "&ldquo;Kamu tidak jujur tentang itu&rdquo;",
+    "&ldquo;Semoga bahagia&rdquo;",
+    "&ldquo;Dan seandainya air jatuh kembali&rdquo;",
+    "&ldquo;Aku tetap di sini Dan akan mengusap kembali air mata itu&rdquo;",
+    "&ldquo;Karena di sini aku tulus mencintaimu&rdquo;"
+  ];
+  
+  const gambar = [
+    "1.gif",
+    "2.gif",
+    "3.gif",
+    "4.gif",
+    "5.gif",
+    "6.gif",
+    "7.gif",
+    "8.gif",
+    "9.gif",
+    "10.gif",
+    "11.gif",
+    "12.gif",
+    "13.gif",
+    "14.gif",
+    "15.gif",
+    "16.gif",
+    "17.gif",
+    "18.gif"
+  ];
+  let index = 0;
+  const kalimatElement = document.getElementById("kalimat");
+  const gambarElement = document.getElementById("gambar");
+  const body = document.body;
+  const audioElement = document.getElementById("audio");
+  
+  gambarElement.addEventListener("click", function() {
+    if (index >= kalimat.length) {
+      index = 0;
+    }
+  
+    kalimatElement.innerHTML = kalimat[index];
+    gambarElement.src = gambar[index];
+    index++;
+  
+    body.style.backgroundColor = "black";
+    audioElement.play();
+  });
